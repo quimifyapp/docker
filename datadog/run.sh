37364ff29f3f4062152ffeb166e2010a4bdba909
Quimify <contacto@quimify.com>
2023-05-30 09:51:15 +0200

    Put Datadog volumes all in one named datadog-volume

--- datadog/run.sh
+++ datadog/run.sh
@@ -1,6 +1,10 @@
 docker image build -t datadog-image .
 
 docker run -d --name datadog-container \
+    --restart unless-stopped \
+    -v datadog-volume:/var/log/datadog \
+    -v datadog-volume:/var/run/s6 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
 	-e DD_REMOTE_CONFIGURATION_ENABLED=true \
 	-e DD_LOGS_ENABLED=true \
     -e DD_SYSTEM_PROBE_NETWORK_ENABLED=true \
@@ -9,11 +13,7 @@ docker run -d --name datadog-container \
     -e DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL=true \
     -e DD_SITE="us5.datadoghq.com" \
     -e DD_CONTAINER_EXCLUDE_LOGS="name:datadog-container" \
-	-v /var/run/docker.sock:/var/run/docker.sock:ro \
-	-v /proc/:/host/proc/:ro \
-	-v /opt/datadog-agent/run:/opt/datadog-agent/run:rw \
-	-v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
-	--security-opt apparmor:unconfined \
+    --security-opt apparmor:unconfined \
     --cap-add=SYS_ADMIN \
     --cap-add=SYS_RESOURCE \
     --cap-add=SYS_PTRACE \
@@ -22,7 +22,7 @@ docker run -d --name datadog-container \
     --cap-add=NET_RAW \
     --cap-add=IPC_LOCK \
     --cap-add=CHOWN \
-	--restart unless-stopped \
     datadog-image
 
 docker logs datadog-container -f
+
