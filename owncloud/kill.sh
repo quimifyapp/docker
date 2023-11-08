19c1ede105ad391d06e6fa4dc0eb18b12478e0d6
Quimify <contacto@quimify.com>
2023-11-08 18:45:52 +0100

    Remove Owncloud

--- owncloud/kill.sh
+++
@@ -1,3 +0,0 @@
-docker stop owncloud-container
-docker rm owncloud-container
-docker rmi owncloud-image
--- owncloud/run.sh
+++
@@ -1,3 +0,0 @@
-docker image build -t owncloud-image .
-docker run -d --name owncloud-container -p 8080:8080 --net quimify-network -v owncloud-volume:/mnt/data --restart unless-stopped -d owncloud-image
-docker logs owncloud-container -f
